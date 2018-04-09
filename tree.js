b9c50230f2ad65208803022129f4923bff100ee1
Peter Heisig <peter.heisig@medien-dresden.de>
2018-04-09 10:06:02 +0200

    initial commit

---
+++ tree.js
@@ -0,0 +1,12 @@
+const con***REMOVED***g = require('./lib/con***REMOVED***g')
+const taiga = require('./lib/taiga')
+const treeFor = require('terminal-tree')
+
+taiga(con***REMOVED***g.baseUrl, con***REMOVED***g.token)
+  .***REMOVED***((data) => {
+    console.log(treeFor(data, {
+      symbol: false,
+      highlight: true,
+      padding: 1
+    ***REMOVED***))
+  ***REMOVED***)
